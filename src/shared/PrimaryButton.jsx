8579862e5fb8271b1396dfd2215e68fb47f8cb8d
developedbyrayhan <islamraihanofficial@gmail.com>
2024-02-21 19:14:41 +0600

    feat: added Error page

--- src/shared/PrimaryButton.jsx
+++ src/shared/PrimaryButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const PrimaryButton = ({ text }) => {
+const PrimaryButton = ({ text, url }) => {
     return (
         <div>
-            <a href="#_" class="relative inline-block px-5 py-3 rounded-sm font-medium group">
+            <a href={url} class="relative inline-block px-5 py-3 rounded-sm font-medium group">
                 <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0 rounded-sm"></span>
                 <span class="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black rounded-sm"></span>
                 <span class="relative text-black group-hover:text-white capitalize text-lg">{text}</span>
